5298cc69ffc2cf8bf694afa63d206578de046886
WasteofSpaceYT <WasteofSpaceYT@outlook.com>
2022-12-15 21:08:45 -0500

    made stuff

---
+++ src/main/java/com/waste/fedimension/world/DimensionTeleporter.java
@@ -0,0 +1,17 @@
+package com.waste.fedimension.world;
+
+import net.minecraft.block.material.Material;
+import net.minecraft.entity.Entity;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.server.ServerWorld;
+import net.minecraftforge.common.util.ITeleporter;
+
+import java.util.Optional;
+import java.util.function.Function;
+
+public class DimensionTeleporter implements ITeleporter {
+
+    public DimensionTeleporter(Optional<BlockPos> bedPosition, boolean b) {
+    }
+}
